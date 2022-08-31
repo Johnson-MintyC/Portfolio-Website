1af0ae52f67985f25c4b29ff90d0fca7a1b53e01
Johnson-MintyC <107607833+Johnson-MintyC@users.noreply.github.com>
2022-08-31 11:53:22 +1000

    alt text to cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,10 @@
 import { Col, Container } from "react-bootstrap"
 
-const ProjectCard = ({title, description, imgUrl, liveLink, gitLink}) => {
+const ProjectCard = ({title, description, imgUrl, altText, liveLink, gitLink}) => {
     return (
     <Col sm={6} md={4}>
         <div className="proj-imgbx"> 
-            <img src={imgUrl} />
+            <img src={imgUrl} alt={altText}/>
             <div className="proj-txtx">
                 <h4>{title}</h4>
                 <span>{description}</span>
