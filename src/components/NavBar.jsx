cf48ecdf2eae5437eecfacbb7cf3bd522fc7bef9
Johnson-MintyC <107607833+Johnson-MintyC@users.noreply.github.com>
2022-08-31 11:39:32 +1000

    updated links removed unused vars for deploy

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -70,10 +70,10 @@ const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#">
+              <a href="https://www.linkedin.com/in/johnson-cho/" target="_blank">
                 <img src={navIcon1} alt="linkedin" />
               </a>
-              <a href="#">
+              <a href="https://github.com/Johnson-MintyC">
                 <img src={navIcon2} alt="" />
               </a>
               <a href="#">
